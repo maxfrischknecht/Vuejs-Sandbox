191f368d4f7ce0610719b1ecfa942daa83a5d2df
Max Frischknecht <max@debutdebut.com>
2020-07-08 17:10:44 +0200

    create simple component example

---
+++ 08-components/vue-cli-components/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Server Status: {{status}}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      status: "Critical"
+    }
+  },
+  methods: {
+    // use ES6 style
+    changeStatus(){
+      this.status = "Normal";
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- 08-components/vue-cli-components/src/main.js
+++ 08-components/vue-cli-components/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// register the component globally
+import Home from './components/Home.vue'
+Vue.component("app-server-status", Home);
+
 Vue.config.productionTip = false
 
 new Vue({
