da4a7c6729424e19e1196cc7f1ec3186d0246b45
Max Frischknecht <max@debutdebut.com>
2020-07-24 14:58:56 +0200

    add exercise 08

---
+++ exercise-8-slots-dynamic-cmp/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+        <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+        <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+        <hr />
+
+        <component :is="selectedComponent">
+          <p>This is the Content</p>
+        </component>
+        <!--<app-blue>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-blue>-->
+        <!--<app-green>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-green>-->
+        <!--<app-red>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-red>-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
+
+export default {
+  data() {
+    return {
+      selectedComponent: "appBlue",
+    };
+  },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ exercise-8-slots-dynamic-cmp/src/components/Green.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        /* border: 1px solid rgb(232, 253, 232); */
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+    h1{
+        color: lightcoral;
+    }
+</style>
