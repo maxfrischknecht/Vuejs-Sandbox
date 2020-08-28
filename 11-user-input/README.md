1a6d140a346d65e103813701f2a2e611a5130f1b
Max Frischknecht <max@debutdebut.com>
2020-08-28 14:18:46 +0200

    v-model with checkboxes

--- 11-user-input/README.md
+++ 11-user-input/README.md
@@ -32,3 +32,18 @@ export default {
 ```
 
 This is realtime! If you only want it to be update on submit or change of field use `v-model.lazy`. Use `v-model.trim` to get rid of white space and `v-model.number` to convert the string to a number.
+
+## Placeholder Content
+
+Always use `v-model` for placeholder content!
+
+## Linebreaks
+
+Set `white-space: pre;` to output multi line text from inputs fields with linebreaks.
+
+## Checkboxes Or saving in an array
+
+If you have an array in data, e.g. `sendMail: []`, and bind input fiels multiple times to this array with `v-model=sendMail`, Vuejs will automatically detect that this is an array and push it.
+
+Then output it with `<li v-for="(item, i) in sendMail" :key="i">{{item}}</li>`
+
--- 11-user-input/form-example/src/App.vue
+++ 11-user-input/form-example/src/App.vue
@@ -24,17 +24,17 @@
           <label for="message">Message</label>
           <br />
           <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea id="message" rows="5" class="form-control"></textarea>
+          <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail"/> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail"/> Send Infomail
             </label>
           </div>
         </div>
@@ -75,12 +75,12 @@
             <p>Mail: {{user.email}}</p>
             <p>Password: {{user.password}}</p>
             <p>Age: {{user.age}}</p>
-            <p>Message:</p>
+            <p style="white-space: pre;">Message: {{message}}</p>
             <p>
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+              <li v-for="(item, i) in sendMail" :key="i">{{item}}</li>
             </ul>
             <p>Gender:</p>
             <p>Priority:</p>
@@ -100,7 +100,9 @@ export default {
         email: "placeholder@gmx.de",
         password: "",
         age: 27
-      }
+      },
+      message: "A new Text",
+      sendMail: []
     };
   },
 };
