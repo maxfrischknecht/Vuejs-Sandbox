218ccf6904170154756a94847abc029494a8ce6b
Max Frischknecht <max@debutdebut.com>
2021-03-10 16:10:29 +0100

    add basic example of store usage

--- 15-vuex/vuex-example/src/main.js
+++ 15-vuex/vuex-example/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import the strore
+import { store } from './store/store'
+
+// add it to the vue instance 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
